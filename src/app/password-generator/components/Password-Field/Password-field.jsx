f33f78866db10c9d11cd6c6c6ee6d5f6dfda8679
Luis Vera <101475330+LuisVera1@users.noreply.github.com>
2024-10-23 12:08:35 -0600

    styles to range and checkboxes

--- src/app/password-generator/components/Password-Field/Password-field.jsx
+++ src/app/password-generator/components/Password-Field/Password-field.jsx
@@ -1,21 +1,50 @@
+'use client'
 import Image from 'next/image';
 import styles from './password.module.css'
+import { useEffect, useState } from 'react';
+
+const appstatus = {
+  start: 'start',
+  generated: 'generated',
+  copied: 'copied',
+}
 
 export default function PasswordField() {
-  const state = 'start'
+  const [status, setStatus] = useState(appstatus.generated)
+
+  //hide COPIED legend
+  useEffect(() => {
+    if (status === appstatus.copied) {
+      const timer = setTimeout(() => setStatus(appstatus.generated), 1500);
+      return () => clearTimeout(timer);
+    }
+  }, [status]);
+  
+  const handleCopyPassword = () => {
+    if (status == appstatus.generated) {
+      //copy password
+      navigator.clipboard.writeText('p4s5w0rd');
+      //update status
+      setStatus(appstatus.copied)
+    }
+  }
 
   return (
     <div className={styles.passFieldSection}>
-      <h2 className={state != 'star' ? styles.passEmpty : styles.passActive}>P4$5W0rD!</h2>
+      <h2 className={status == appstatus.start ? styles.passEmpty : styles.passActive}>P4$5W0rD!</h2>
 
       <div className={styles.btnCopiedSection}>
-        {state != 'start' && <p className={styles.copied}>COPIED</p>}
-        <Image
-          src="password-generator/icon-copy.svg"
-          width={21}
-          height={24}
-          alt="Copy password"
-        />
+        {status == appstatus.copied && <p className={styles.copied}>COPIED</p>}
+
+        <button className={styles.btnCopy} onClick={handleCopyPassword} aria-label='Copy password'>
+          <Image
+            src="password-generator/icon-copy.svg"
+            width={21}
+            height={24}
+            alt="Copy password"
+          />
+        </button>
+
       </div>
 
     </div>
--- src/app/password-generator/components/Password-Options/PasswordOptions.jsx
+++ src/app/password-generator/components/Password-Options/PasswordOptions.jsx
@@ -1,28 +1,61 @@
+'use client'
+
+import { useState } from 'react'
 import styles from './content.module.css'
 
+const initialState = {
+  length: 10,
+}
+
 export default function PasswordOptions() {
+  const [options, setOptions] = useState(initialState);
+
+  const handleChange = (event) => {
+    const element = event.target.name;
+    const value = event.target.value;
+    setOptions({ ...options, [element]: value })
+  }
+
+
   return (
-    <>
+    <div className={styles.content}>
 
-      <div className={styles.content}>
 
-        <div>
+      {/* length */}
+      <div className={styles.lengthSection}>
+        <p className={styles.labelLength}>
           Character Length
-          [] Include Uppercase Letters
-          [] Include Lowercase Letters
-          [] Include Numbers
-          [] Include Symbols
-        </div>
+        </p>
+        <p className={styles.lengthIndicator}>{options.length}</p>
+      </div>
+
+      {/* range */}
+      <input
+        className={styles.inpRange}
+        type="range"
+        name="length"
+        min="5"
+        max="15"
+        step="1"
+        value={options.length}
+        onChange={handleChange}
+      />
 
 
+      {/* checkboxes */}
 
-        <div className={styles.strengthSection}>
-          Strength
-        </div>
+      <div className={styles.checkboxesSection}>
+        <label htmlFor='uppercase'>
+          <input className={styles.inputBox} type="checkbox" name="uppercase" id="uppercase" />
+          <div className={styles.boxIndicator}></div>
+          Include Uppercase Letters</label>
 
-        <button className={styles.button}>Generate</button>
 
+      </div>
 
+      <div>
+        <input type="checkbox" name="lowercase" id="lowercase" />
+        <label htmlFor='lowercase'>Include Lowercase Letters</label>
       </div>
 
 
@@ -30,8 +63,45 @@ export default function PasswordOptions() {
 
 
 
+      {/* working */}
+
+      <div>
+
+        [] Include Numbers
+        [] Include Symbols
+      </div>
 
 
-    </>
+
+      <div className={styles.strengthSection}>
+        <p className={styles.strengthLabel}>Strength</p>
+
+        <div className={styles.newComponent}>
+          <p className={styles.strengthScore}>STRONG</p>
+          <div className={styles.indicator}>
+            <div className={styles.strength1}></div>
+            <div className={styles.strength2}></div>
+            <div className={styles.strength3}></div>
+            <div className={styles.strength4}></div>
+          </div>
+
+
+        </div>
+
+      </div>
+
+      <button className={styles.button}>Generate</button>
+
+
+    </div>
+
+
+
+
+
+
+
+
+
   )
 }

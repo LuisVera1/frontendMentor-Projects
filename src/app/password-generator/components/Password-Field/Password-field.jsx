3ebff2d9caa66c1a1779199848b78c85958ced15
Luis Vera <101475330+LuisVera1@users.noreply.github.com>
2024-10-24 11:55:50 -0600

    add styles to checkboxes
    -styles to checkboxes according to design
    -add color changing to button copy

--- src/app/password-generator/components/Password-Field/Password-field.jsx
+++ src/app/password-generator/components/Password-Field/Password-field.jsx
@@ -1,7 +1,7 @@
 'use client'
-import Image from 'next/image';
 import styles from './password.module.css'
 import { useEffect, useState } from 'react';
+import Copy from '@/app/password-generator/components/Icons/Copy'
 
 const appstatus = {
   start: 'start',
@@ -10,16 +10,16 @@ const appstatus = {
 }
 
 export default function PasswordField() {
-  const [status, setStatus] = useState(appstatus.generated)
+  const [status, setStatus] = useState(appstatus.start)
 
-  //hide COPIED legend
+  //hide COPIED legend after 1500ms
   useEffect(() => {
     if (status === appstatus.copied) {
       const timer = setTimeout(() => setStatus(appstatus.generated), 1500);
       return () => clearTimeout(timer);
     }
   }, [status]);
-  
+
   const handleCopyPassword = () => {
     if (status == appstatus.generated) {
       //copy password
@@ -37,12 +37,7 @@ export default function PasswordField() {
         {status == appstatus.copied && <p className={styles.copied}>COPIED</p>}
 
         <button className={styles.btnCopy} onClick={handleCopyPassword} aria-label='Copy password'>
-          <Image
-            src="password-generator/icon-copy.svg"
-            width={21}
-            height={24}
-            alt="Copy password"
-          />
+          <Copy />
         </button>
 
       </div>
--- src/app/password-generator/components/Password-Options/PasswordOptions.jsx
+++ src/app/password-generator/components/Password-Options/PasswordOptions.jsx
@@ -5,21 +5,35 @@ import styles from './content.module.css'
 
 const initialState = {
   length: 10,
+  uppercase: false,
+  lowercase: false,
+  numbers: false,
+  symbols: false,
 }
 
 export default function PasswordOptions() {
   const [options, setOptions] = useState(initialState);
 
-  const handleChange = (event) => {
+  const handleRange = (event) => {
     const element = event.target.name;
     const value = event.target.value;
     setOptions({ ...options, [element]: value })
   }
 
+  const handleCheck = (event) => {
+    const element = event.target.name;
+    const value = !options[element]
+    setOptions({ ...options, [element]: value })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submitting')
+    console.log(options)
+  }
 
   return (
-    <div className={styles.content}>
-
+    <form className={styles.content} onSubmit={handleSubmit}>
 
       {/* length */}
       <div className={styles.lengthSection}>
@@ -38,40 +52,46 @@ export default function PasswordOptions() {
         max="15"
         step="1"
         value={options.length}
-        onChange={handleChange}
+        onChange={handleRange}
       />
 
-
       {/* checkboxes */}
-
       <div className={styles.checkboxesSection}>
-        <label htmlFor='uppercase'>
-          <input className={styles.inputBox} type="checkbox" name="uppercase" id="uppercase" />
-          <div className={styles.boxIndicator}></div>
-          Include Uppercase Letters</label>
+        <div className={styles.checkRow}>
+          <label className={styles.checkboxLabel}>
+            <input className={styles.inputBox} type="checkbox" name="uppercase" checked={options.uppercase} onChange={handleCheck} />
+            <div className={styles.boxIndicator}></div>
+            Include Uppercase Letter
+          </label>
+        </div>
 
+        <div className={styles.checkRow}>
+          <label className={styles.checkboxLabel}>
+            <input className={styles.inputBox} type="checkbox" name="lowercase" checked={options.lowercase} onChange={handleCheck} />
+            <div className={styles.boxIndicator}></div>
+            Include Lowercase Letters
+          </label>
+        </div>
 
+        <div className={styles.checkRow}>
+          <label className={styles.checkboxLabel}>
+            <input className={styles.inputBox} type="checkbox" name="numbers" checked={options.numbers} onChange={handleCheck} />
+            <div className={styles.boxIndicator}></div>
+            Include Numbers
+          </label>
+        </div>
+
+        <div className={styles.checkRow}>
+          <label className={styles.checkboxLabel}>
+            <input className={styles.inputBox} type="checkbox" name="symbols" checked={options.symbols} onChange={handleCheck} />
+            <div className={styles.boxIndicator}></div>
+            Include Symbols
+          </label>
+        </div>
       </div>
 
-      <div>
-        <input type="checkbox" name="lowercase" id="lowercase" />
-        <label htmlFor='lowercase'>Include Lowercase Letters</label>
-      </div>
 
-
-
-
-
-
-      {/* working */}
-
-      <div>
-
-        [] Include Numbers
-        [] Include Symbols
-      </div>
-
-
+      {/* compoment */}
 
       <div className={styles.strengthSection}>
         <p className={styles.strengthLabel}>Strength</p>
@@ -84,24 +104,12 @@ export default function PasswordOptions() {
             <div className={styles.strength3}></div>
             <div className={styles.strength4}></div>
           </div>
-
-
         </div>
 
       </div>
+      {/* end component */}
 
-      <button className={styles.button}>Generate</button>
-
-
-    </div>
-
-
-
-
-
-
-
-
-
+      <button className={styles.button} type='submit'>Generate</button>
+    </form>
   )
 }
